be2ce7d1330857144f63b0c803118c13070ad1b5
Yasuaki Shimura <yshimura1001@gmail.com>
2026-03-22 16:17:42 +0900

    コメントを追記。

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,10 +4,13 @@ import type { Ref } from "vue"
 import { clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+// CSSのクラス名を結合しつつ、Tailwindのクラス名の競合を解決する関数。(重複した場合、後者を優先。)
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
+// TanStack TableのUpdater<T>に対応するためのヘルパー関数。
+// 値そのもの、または「古い値を受け取って新しい値を返す関数」のどちらも受け入れ、Refの値を更新。
 export function valueUpdater<T extends Updater<any>>(updaterOrValue: T, ref: Ref) {
   ref.value
     = typeof updaterOrValue === "function"
